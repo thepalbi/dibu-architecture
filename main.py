3cc67ba91134542385ef7329ee18b14bf04f4405
Pablo Balbi <pablo.l.balbi@gmail.com>
2023-05-31 20:42:37 -0300

    wip2

--- main.py
+++ main.py
@@ -1,30 +1,89 @@
-from lark import Lark, Tree
-from lark.lexer import Token
+from lark import Lark, Visitor, Tree, Token
+from dataclasses import dataclass
+from typing import List, Dict
 
 # https://github.com/antlr/grammars-v4/blob/master/asm/asm8086/asm8086.g4
 
-with open("grammar.txt", "r") as f:
+INSTRUCTION_SIZE = 16
+
+
+class ProgramVisitor(Visitor):
+    def __init__(self) -> None:
+        self._labels = {}
+        self._instructions = []
+        self._comments = []
+        self._seen_instruction_lines = 0
+        super().__init__()
+
+    def code(self, code_tree):
+        code_visitor = CodeLineVisitor()
+        # visit code and create instruction
+        code_visitor.visit_topdown(code_tree)
+        self._instructions.append(code_visitor.produce_instruction())
+        # record that we've seen the instruction after visiting the label
+        self._seen_instruction_lines += 1
+
+    def comment(self, c: Tree):
+        self._comments.append(c.children[0].value)
+
+    def label(self, l: Tree):
+        # when visiting the label, take as the amount of seen lines one less,
+        # since we've visited first the line item, and then the label
+        # todo(pablo): this is not working that good
+        self._labels[l.children[0].value] = (
+            self._seen_instruction_lines - 1) * INSTRUCTION_SIZE
+
+    def produce_program(self):
+        return Program(
+            instructions=self._instructions,
+            labels=self._labels,
+        )
+
+
+@dataclass
+class Instruction:
+    opcode: str
+
+
+@dataclass
+class Program:
+    instructions: List[Instruction]
+    labels: Dict[str, int]
+
+
+class CodeLineVisitor(Visitor):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def opcode(self, o: Tree):
+        self._opcode = o.children[0].value
+
+    def produce_instruction(self) -> Instruction:
+        return Instruction(
+            opcode=self._opcode,
+        )
+
+
+with open("grammar.lark", "r") as f:
     grammar = f.read()
 
-parser = Lark(grammar)
+parser = Lark(grammar, start="prog")
 
 example = """loop: mov r1 r2
 // the instruction below generates a random number
+mov r1 r2
+loop: mov r2 r1
 rnd r1
 mov r1 b101
+jmp loop
 """
 
 parsed_tree = parser.parse(example)
 print(parsed_tree.pretty())
 
 print("compiling tree")
+visitor = ProgramVisitor()
+visitor.visit_topdown(parsed_tree)
 
-# first, prog contains a series of lines, each can be code or comment
-for line in parsed_tree.children:
-    for ch in line.children:
-        print(ch)
-        # token: Token = line
-        # match token.type:
-        #     case 'comment_line': print("comment")
-        #     case 'code': print("code")
-        #     case _: print("unsupported type: %s" % (token.type))
+program = visitor.produce_program()
+print(program)
