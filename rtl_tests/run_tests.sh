0c2350137a1e0765e26f37d3ea7df629a67d2589
Pablo Balbi <pablo.l.balbi@gmail.com>
2023-06-21 20:02:39 -0300

    run single test

--- rtl_tests/run_tests.sh
+++ rtl_tests/run_tests.sh
@@ -4,8 +4,18 @@ set -eufo pipefail
 # tests=$(find . -name "test_*.py")
 tests="./test_datapath.py"
 
+if [[ "$#" == "1" ]]; then
+    TESTS=$1
+fi
+
+
 for test in $tests; do
     echo "RUNNING TEST MODULE: $test"
     module=`echo $test|sed -E 's/\.\/(.+)\.py/\1/g'`
-    MODULE=$module make regression
+    if [ "${TESTS:-}" != "" ]; then
+        echo "running single test: $TESTS"
+        TESTS=$TESTS MODULE=$module make regression
+    else
+        MODULE=$module make regression
+    fi
 done
