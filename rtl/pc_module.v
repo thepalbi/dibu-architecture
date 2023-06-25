85cd70c2d168a2db523a563624950fb0c8dab9b2
ronapa <parra.rodrigo.n@gmail.com>
2023-06-24 23:53:30 -0300

    Added initialization and fixed sintax

--- rtl/pc_module.v
+++ rtl/pc_module.v
@@ -1,36 +1,61 @@
-`timescale 1ns / 1ps
-
-module pc_module(clk, pc, inc, inc_pc_ref, dec_pc_ref, pc_out, err);
+module pc_module(clk, inc, inc_pc_ref, dec_pc_ref, pc_set, pc_set_value, pc_out, err);
     // utility signals
     input clk;
 
-    // control signals when using as dut
-    input inc, inc_pc_ref, dec_pc_ref;
+    input inc, inc_pc_ref, dec_pc_ref, pc_set;
+    input [8:0] pc_set_value;
 
     // outputs
-    output reg [8:0] pc_out;
+    output wire [8:0] pc_out;
     output reg err;
 
     reg [2:0] pc_ref;
     reg [8:0] pc_bank [0:7];
+    
+    initial
+    begin
+       pc_bank[0] = 9'd0;
+       pc_bank[1] = 9'd0;
+       pc_bank[2] = 9'd0;
+       pc_bank[3] = 9'd0;
+       pc_bank[4] = 9'd0;
+       pc_bank[5] = 9'd0;
+       pc_bank[6] = 9'd0;
+       pc_bank[7] = 9'd0;
+       
+       pc_ref = 3'd0;
+       err = 0;
+    end
 
     always @ (posedge clk) begin
-        // we should be able to read the current value + increment in one clk rising edge
-        if (inc)
+        if (inc) begin
             pc_bank[pc_ref] <= pc_bank[pc_ref] + 1;
-        if (inc_pc_ref)
-            if (pc_ref == 3'd7)
-                err <= 1
-            else
-                pc_ref <= pc_ref + 1
-        if (dec_pc_ref)
-            if (pc_ref == 3'd0)
-                err <= 1
-            else
-                pc_ref <= pc_ref - 1
+        end
+        
+        if (pc_set) begin
+            pc_bank[pc_ref] <= pc_set_value;
+        end
+        
+        if (inc_pc_ref) begin
+            if (pc_ref == 3'd7) begin
+                err <= 1;
+            end
+            else begin
+                pc_ref <= pc_ref + 1;
+            end 
+        end
+        
+        if (dec_pc_ref) begin
+            if (pc_ref == 3'd0) begin
+                err <= 1;
+            end
+            else begin
+                pc_ref <= pc_ref - 1;
+            end
+        end
     end
 
     // combinational assings
-    assign pc_out = pc_bank[pc_ref]
+    assign pc_out = pc_bank[pc_ref];
     
 endmodule
